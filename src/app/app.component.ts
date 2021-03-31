a10f19b45eddfe19fc075f9d4be7ba59d9e55d05
theduodecim <juansebastian.012@gmail.com>
2021-03-31 11:56:07 -0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import print from 'print-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,895 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PrintjsLib';
+  someJSONdata = [
+    {
+      name: 'John Doe',
+      email: 'john@doe.com',
+      phone: '111-111-1111'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    },
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+    ,
+    {
+      name: 'Barry Allen',
+      email: 'barry@flash.com',
+      phone: '222-222-2222'
+    },
+    {
+      name: 'Cool Dude',
+      email: 'cool@dude.com',
+      phone: '333-333-3333'
+    }
+
+  ];
+
+  printJS() {
+    // https://printjs.crabbly.com/
+    //HTML PRINT
+    print({
+      printable: 'myElementId',
+      documentTitle: 'PDF',
+      repeatTableHeader: true,
+      type: 'html',
+      style: '.custom-h3 { color: red; }'
+    });
+
+    //JSON PRINT
+    /*
+    print({
+      documentTitle: 'PDF',
+      printable: this.someJSONdata,
+      repeatTableHeader: true,
+      type: 'json',
+      properties: ['name', 'email', 'phone'],
+      header: '<h3 class="custom-h3">My custom header</h3>',
+      style: '.custom-h3 { color: red; }'
+    });
+*/
+  }
+
 }
